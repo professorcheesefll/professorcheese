874e61b8efed20249fc2da5780ecbb3f01250dc9
Professor_Cheese <annaji.garimella6@gmail.com>
2025-12-23 11:48:32 -0500

    Add files via upload

---
+++ gear-ratio-calculator.js
@@ -0,0 +1,30 @@
+function gcd(a, b) {
+    while (b !== 0) {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+
+document.getElementById("calculateRatio").addEventListener("input", function () {
+    const driver = Number(document.getElementById("driverTeeth").value);
+    const follower = Number(document.getElementById("followerTeeth").value);
+    if (driver <= 0 || follower <= 0) {
+        document.getElementById("ratioResult").textContent =
+
+        "Please enter valid gear tooth counts.";
+    return;
+    }
+
+    const divisor = gcd(driver, follower);
+    const simplifiedDriver = driver / divisor;
+    const simplifiedFollower = follower / divisor;
+
+    document.getElementById("ratioResult").textContent =
+
+    `Gear ratio (driver : follower) = ${simplifiedDriver}:${simplifiedFollower}`;
+    //okay so this line above can actually be just "Gear ratio = " + simplifiedDriver + ":" + simplifiedFollower
+    
+});
